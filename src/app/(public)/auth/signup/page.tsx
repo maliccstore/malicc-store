ee9fd3a5238c42fb2843b13821d2f9b08ef97718
Shahzer Baig <shahzer.baig@gmail.com>
2025-08-09 01:42:48 +0530

    Modified(Signup): Dark mode compatible

--- src/app/(public)/auth/signup/page.tsx
+++ src/app/(public)/auth/signup/page.tsx
@@ -3,11 +3,7 @@ import { Flex } from '@radix-ui/themes';
 
 export default function SignupPage() {
   return (
-    <Flex
-      justify="center"
-      align="center"
-      className="min-h-screen bg-gray-50 p-4"
-    >
+    <Flex justify="center" align="center" className="min-h-screen p-4">
       <SignupForm />
     </Flex>
   );
