204faf269c6437e10ce71bdb443772d977bb1cee
Shahzer Baig <shahzer.baig@gmail.com>
2025-08-09 01:42:04 +0530

    Modified(login): Layout of Login

--- src/app/(public)/auth/login/page.tsx
+++ src/app/(public)/auth/login/page.tsx
@@ -3,11 +3,7 @@ import LoginForm from '@/components/login/LoginForm';
 
 export default function LoginPage() {
   return (
-    <Flex
-      justify="center"
-      align="center"
-      className="min-h-screen bg-gray-50 p-4"
-    >
+    <Flex justify="center" align="center" className="min-h-screen  p-4">
       <LoginForm />
     </Flex>
   );
