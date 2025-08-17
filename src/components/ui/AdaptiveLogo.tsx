fd4b2cb6b332469bdac82fc3d12ad99f94fe6181
Shahzer Baig <shahzer.baig@gmail.com>
2025-08-17 19:19:21 +0530

    Update(Adaptive Logo): Logo turns color according to theme

--- src/components/ui/AdaptiveLogo.tsx
+++ src/components/ui/AdaptiveLogo.tsx
@@ -1,25 +1,28 @@
-'use client';
+import { RootState } from '@/store';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+
+import { useSelector } from 'react-redux';
 
 export default function AdaptiveLogo() {
   // Dark Mode
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const theme = useSelector((state: RootState) => state.app.theme);
 
-  useEffect(() => {
-    // Check for dark mode preference
-    const darkModeMediaQuery = window.matchMedia(
-      '(prefers-color-scheme: dark)'
-    );
-    setIsDarkMode(darkModeMediaQuery.matches);
-  }, []);
+  const isThemeDark = () => {
+    if (theme == 'light') {
+      return false;
+    } else {
+      return true;
+    }
+  };
 
   return (
     <>
-      {/* Conditional logo based on theme */}
-      {!isDarkMode ? (
+      {
+        //Conditional logo based on theme
+      }
+      {isThemeDark() ? (
         <Image
-          src={'/assets/images/maliccwhite.svg'}
+          src={'/assets/images/maliccwhite.png'}
           priority={true}
           alt="malicc.store"
           width={100}
