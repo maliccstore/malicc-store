f8fc50820c4a6d7f451ded19e89577c252c2001a
Shahzer Baig <shahzer.baig@gmail.com>
2025-12-12 15:03:13 +0530

    Updated(text)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Link href="/explore" style={{ color: 'blue' }}>
-          Explore
+          Explore me
         </Link>
 
         <BlogPage />
