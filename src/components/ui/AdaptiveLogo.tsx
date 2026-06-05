fc14c92c260ec36cb19b7ad8c50b92d2d543614f
Shah <41285362+shahzerbaig@users.noreply.github.com>
2026-06-05 17:19:55 +0530

    fix(header file): Image component is removed and generic img is being used

--- src/components/ui/AdaptiveLogo.tsx
+++ src/components/ui/AdaptiveLogo.tsx
@@ -1,6 +1,4 @@
 import { RootState } from '@/store';
-import Image from 'next/image';
-
 import { useSelector } from 'react-redux';
 
 interface AdaptiveLogoProps {
@@ -8,33 +6,74 @@ interface AdaptiveLogoProps {
   previewUrl?: string;
 }
 
-export default function AdaptiveLogo({ previewWidth, previewUrl }: AdaptiveLogoProps = {}) {
-  // Dark Mode
+export default function AdaptiveLogo({
+  previewWidth,
+  previewUrl,
+}: AdaptiveLogoProps = {}) {
   const theme = useSelector((state: RootState) => state.app.theme);
-  const appearance = useSelector((state: RootState) => state.adminAppearance?.settings);
+  const appearance = useSelector(
+    (state: RootState) => state.adminAppearance?.settings
+  );
 
   const isThemeDark = () => {
-    if (theme == 'light') {
-      return false;
-    } else {
-      return true;
-    }
+    return theme !== 'light';
   };
 
-  const defaultLogo = isThemeDark() ? '/assets/images/maliccwhite.png' : '/assets/images/malicc.svg';
+  const defaultLogo = isThemeDark()
+    ? '/assets/images/maliccwhite.png'
+    : '/assets/images/malicc.svg';
+
   const logoUrl = previewUrl || appearance?.logo_url || defaultLogo;
   const logoWidth = previewWidth || appearance?.logo_width || 100;
 
   return (
-    <>
-      <Image
-        src={logoUrl}
-        priority={true}
-        alt={appearance?.store_name || "malicc.store"}
-        width={logoWidth}
-        height={logoWidth}
-        style={{ objectFit: 'contain' }}
-      />
-    </>
+    <img
+      src={logoUrl}
+      alt={appearance?.store_name || 'malicc.store'}
+      width={logoWidth}
+      height={logoWidth}
+      style={{ objectFit: 'contain' }}
+    />
   );
 }
+
+// import { RootState } from '@/store';
+// import Image from 'next/image';
+
+// import { useSelector } from 'react-redux';
+
+// interface AdaptiveLogoProps {
+//   previewWidth?: number;
+//   previewUrl?: string;
+// }
+
+// export default function AdaptiveLogo({ previewWidth, previewUrl }: AdaptiveLogoProps = {}) {
+//   // Dark Mode
+//   const theme = useSelector((state: RootState) => state.app.theme);
+//   const appearance = useSelector((state: RootState) => state.adminAppearance?.settings);
+
+//   const isThemeDark = () => {
+//     if (theme == 'light') {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   };
+
+//   const defaultLogo = isThemeDark() ? '/assets/images/maliccwhite.png' : '/assets/images/malicc.svg';
+//   const logoUrl = previewUrl || appearance?.logo_url || defaultLogo;
+//   const logoWidth = previewWidth || appearance?.logo_width || 100;
+
+//   return (
+//     <>
+//       <Image
+//         src={logoUrl}
+//         priority={true}
+//         alt={appearance?.store_name || "malicc.store"}
+//         width={logoWidth}
+//         height={logoWidth}
+//         style={{ objectFit: 'contain' }}
+//       />
+//     </>
+//   );
+// }
